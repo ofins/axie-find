090438402c0f8ef52ae28c582840000f5755e156
ofins <jkyeewang@gmail.com>
2024-02-09 17:19:34 +0800

    [fix] Error - show wait text for server spin up

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import RunningWithErrorsIcon from "@mui/icons-material/RunningWithErrors";
 
 export default function Error(props) {
+  console.log(props.error.message);
   return (
     <div className="flex gap-6px">
       <RunningWithErrorsIcon />
-      <span>{props.error.message ?? "An error has occurred."}</span>
+      <span>
+        Please be patient while the server is spinning up. You may try
+        refreshing the page again in one minute.
+      </span>
     </div>
   );
 }
