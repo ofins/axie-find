07f05fa7b3dadba6d089f114873b5175b7afaaa4
ofins <jkyeewang@gmail.com>
2024-02-10 19:02:58 +0800

    [fix] Home, menuSetting - update menu and update css

--- src/settings/menuSetting.ts
+++ src/settings/menuSetting.ts
@@ -1,12 +1,17 @@
 export const sideNavList = [
   {
-    text: "Lands",
-    path: "lands",
+    text: "Land Sales",
+    path: "lands/sales",
     icon: "",
   },
   {
-    text: "Genkai",
-    path: "genkai",
+    text: "Genkai Auctions",
+    path: "genkai/auctions",
+    icon: "",
+  },
+  {
+    text: "Cocochoco Sales",
+    path: "items/sales",
     icon: "",
   },
 ];
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,9 +5,11 @@ import { sideNavList } from "@/settings/menuSetting";
 
 const Home = () => {
   return (
-    <div className="h-100vh w-full flex flex-col justify-center gap-20px items-center">
-      <div className="text-24px">Welcome to AxieFind</div>
-      <div className="flex flex-col gap-14px">
+    <div className="h-100vh w-full flex flex-col justify-center gap-50px items-center">
+      <div className="text-36px">
+        Welcome to <strong>AxieFind</strong>
+      </div>
+      <div className="grid grid-cols-3 gap-14px pb-50px">
         {sideNavList.map((item, index) => {
           return (
             <NavLink
@@ -15,7 +17,7 @@ const Home = () => {
               key={index}
               className="decoration-none text-unset"
             >
-              <div className="text-center cursor-pointer hover:translate-x-10 transition-duration-700 transition-ease-in-out ">
+              <div className="text-center cursor-pointer hover:translate-y--3 transition-duration-700 transition-ease-in-out">
                 <Paper
                   sx={{
                     height: 70,
