d9883967b9f62b38d0031962ef8fa48db6ab3a3f
ofins <jkyeewang@gmail.com>
2024-02-10 18:09:16 +0800

    [feat] items route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import "./index.css";
 import "virtual:uno.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LayoutBase from "./components/LayoutBase";
-import LandsLayout from "@/views/layouts/LandsLayout";
 import LandsSales from "@/views/lands/LandsSales";
 import LandsAuctions from "@/views/lands/LandsAuctions";
-import GenkaiLayout from "@/views/layouts/GenkaiLayout";
 import GenkaiSales from "@/views/genkai/GenkaiSales";
 import GenkaiAuctions from "@/views/genkai/GenkaiAuctions";
+import BasicLayout from "@/views/layouts/BasicLayout";
+import ItemSales from "@/views/items/ItemSales";
 import Home from "@/views/Home";
 import { lightTheme, darkTheme } from "./styles/material/theme";
 import { useSelector } from "react-redux";
@@ -29,11 +29,18 @@ function App() {
         <Routes>
           <Route path="/" element={<LayoutBase />}>
             <Route index element={<Home />} />
-            <Route path="/lands" element={<LandsLayout />}>
+
+            <Route path="/lands" element={<BasicLayout />}>
               <Route path="sales" element={<LandsSales />} />
               <Route path="auctions" element={<LandsAuctions />} />
             </Route>
-            <Route path="/genkai" element={<GenkaiLayout />}>
+
+            <Route path="/items" element={<BasicLayout />}>
+              <Route path="sales" element={<ItemSales />} />
+              {/* <Route path="auctions" element={<LandsAuctions />} /> */}
+            </Route>
+
+            <Route path="/genkai" element={<BasicLayout />}>
               <Route path="sales" element={<GenkaiSales />} />
               <Route path="auctions" element={<GenkaiAuctions />} />
             </Route>
