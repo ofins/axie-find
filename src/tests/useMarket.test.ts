c88ce734e29eab8dad766f48a0ad4759385b4edb
ofins <jkyeewang@gmail.com>
2024-02-17 00:35:01 +0800

    [test] - useMarket - useItem - fetchItemSalesData

--- src/tests/useMarket.test.ts
+++ src/tests/useMarket.test.ts
@@ -1,8 +1,53 @@
-import { describe, test, expect, it, jest } from "@jest/globals";
-import { sum } from "../hooks/useMarket";
+import {
+  describe,
+  test,
+  expect,
+  it,
+  jest,
+  beforeAll,
+  afterAll,
+  beforeEach,
+  afterEach,
+} from "@jest/globals";
+import { useItem } from "../hooks/useMarket";
+import { fetchAxieMarketData } from "../api/axieMarketPlace";
+import { renderHook, waitFor } from "@testing-library/react";
 
-describe("sum module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+jest.mock("../api/axieMarketPlace", () => ({
+  fetchAxieMarketData: jest.fn(),
+}));
+
+describe("useItem", () => {
+  describe("fetchItemSalesData", () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should fetch item sales data and update state correctly", async () => {
+      const mockData = ["item1", "item2"];
+      (fetchAxieMarketData as jest.Mock).mockResolvedValueOnce({
+        data: mockData,
+      });
+
+      const { result } = renderHook(() => useItem());
+
+      expect(result.current.loading).toBeTruthy();
+      expect(result.current.errorMessage).toBeNull();
+      expect(result.current.itemLists).toEqual([]);
+
+      await waitFor(() => result.current.fetchItemSalesData());
+
+      expect(fetchAxieMarketData).toHaveBeenCalledWith({
+        queryType: "erc1155TokenSalesQuery",
+        variables: {
+          size: 500,
+          tokenType: "Consumable",
+        },
+      });
+
+      expect(result.current.loading).toBeFalsy();
+      expect(result.current.errorMessage).toBeNull();
+      expect(result.current.itemLists).toEqual(mockData);
+    });
   });
 });
