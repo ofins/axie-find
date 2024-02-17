31999bf9519b0ed4b620ddfca164a73c6b8fd072
ofins <jkyeewang@gmail.com>
2024-02-18 00:11:01 +0800

    [test] useMarket - useGenkai, createGenkaiSalesChartData

--- src/tests/useMarket.test.ts
+++ src/tests/useMarket.test.ts
@@ -6,13 +6,96 @@ import {
   afterEach,
   beforeEach,
 } from "@jest/globals";
-import { useExchangeRate, useItem } from "../hooks/useMarket";
+import { useExchangeRate, useItem, useGenkai } from "@/hooks/useMarket";
 import { fetchAxieMarketData } from "@/api/axieMarketPlace";
+import { fetchMavisMarketData } from "@/api/mavisMarketPlace";
 import { renderHook, waitFor } from "@testing-library/react";
+import { formatMoney } from "@/util/formatMoney";
 
 jest.mock("@/api/axieMarketPlace", () => ({
   fetchAxieMarketData: jest.fn(),
 }));
+
+jest.mock("@/api/mavisMarketPlace", () => ({
+  fetchMavisMarketData: jest.fn(),
+}));
+
+describe("useGenkai", () => {
+  describe("fetchGenkaiMarketData", () => {
+    let result;
+
+    beforeEach(() => {
+      result = renderHook(() => useGenkai()).result;
+
+      expect(result.current.genkaiLists).toEqual([]);
+      expect(result.current.loading).toBeTruthy();
+      expect(result.current.errorMessage).toBeNull();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it("should fetch data and update states correctly", async () => {
+      const mockData = ["item1", "item2"];
+      // const mockDataType = "dataTypeA"
+      (fetchMavisMarketData as jest.Mock).mockResolvedValueOnce({
+        data: mockData,
+      });
+
+      await waitFor(() => result.current.fetchGenkaiMarketData());
+
+      expect(result.current.genkaiLists).toEqual(mockData);
+      expect(result.current.loading).toBeFalsy();
+      expect(result.current.errorMessage).toBeNull();
+    });
+
+    it("should handle errors correctly and states to update correctly", async () => {
+      const mockError = new Error("fetch failed");
+      (fetchMavisMarketData as jest.Mock).mockRejectedValueOnce(mockError);
+
+      try {
+        await waitFor(() => result.current.fetchGenkaiMarketData());
+      } catch (error) {
+        expect(error).toBe(mockError);
+      }
+      expect(result.current.errorMessage).toBe(mockError);
+      expect(result.current.loading).toBeFalsy();
+      expect(result.current.genkaiLists).toEqual([]);
+    });
+
+    it("should handle empty responses correctly", async () => {
+      (fetchMavisMarketData as jest.Mock).mockResolvedValueOnce({ data: [] });
+      await waitFor(() => result.current.fetchGenkaiMarketData());
+      expect(result.current.genkaiLists).toEqual([]);
+    });
+  });
+
+  describe("createGenkaiSalesChartData", () => {
+    const { result } = renderHook(() => useGenkai());
+
+    it.skip("should create sales chart data correctly", () => {
+      const mockGenkaiLists = [
+        { timestamp: "2024-02-16", realPrice: 100, txHash: "hash1" },
+        { timestamp: "2024-02-17", realPrice: 150, txHash: "hash2" },
+      ];
+
+      result.current.genkaiLists = mockGenkaiLists;
+
+      const expectedChartData = [
+        { x: "2024-02-16", y: 100, id: "hash1" },
+        { x: "2024-02-17", y: 150, id: "hash2" },
+      ];
+
+      // const chartData = result.current.createGenkaiSalesChartData();
+      // console.log(chartData);
+
+      expect(result.current.createGenkaiSalesChartData()).toEqual(
+        expectedChartData,
+      );
+    });
+  });
+});
 
 describe("useExchangeRate", () => {
   describe("getExchangeRates", () => {
@@ -41,14 +124,14 @@ describe("useExchangeRate", () => {
       expect(result.current.loading).toBeFalsy();
     });
 
-    it.skip("should handle errors correctly", async () => {
+    it("should handle errors correctly", async () => {
       const mockError = new Error("fetched error");
       (fetchAxieMarketData as jest.Mock).mockRejectedValueOnce(mockError);
 
       try {
-        await waitFor(() => result.current.fetchItemSalesData());
+        await waitFor(() => result.current.getExchangeRates());
       } catch (error) {
-        expect(error).toBe(mockError);
+        expect(error).toEqual(mockError);
       }
 
       expect(fetchAxieMarketData).toHaveBeenCalledWith({
@@ -56,6 +139,12 @@ describe("useExchangeRate", () => {
       });
 
       expect(result.current.loading).toBeFalsy();
+    });
+
+    it("should handle empty responses correctly", async () => {
+      (fetchAxieMarketData as jest.Mock).mockResolvedValueOnce({ data: [] });
+      await waitFor(() => result.current.getExchangeRates());
+      expect(result.current.exchangeRate).toEqual([]);
     });
   });
 });
@@ -123,9 +212,7 @@ describe("useItem", () => {
 
     it("should handle empty response correctly", async () => {
       (fetchAxieMarketData as jest.Mock).mockResolvedValueOnce({ data: [] });
-
       await waitFor(() => result.current.fetchItemSalesData());
-
       expect(result.current.itemLists).toEqual([]);
     });
   });
